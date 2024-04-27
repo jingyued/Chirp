33c271020790d6a9cba19288d048c9b67a4ec32b
jingyued <jingyued@uci.edu>
2024-04-26 19:41:06 -0700

    bugfix/joyce/delete redundant navbar

--- Chirp/src/app/app.module.ts
+++ Chirp/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
   ],
   imports: [
     BrowserModule,
