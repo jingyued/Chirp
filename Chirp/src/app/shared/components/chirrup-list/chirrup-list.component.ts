2242708453aa3b42ae24b111deb72bec7f0577fe
jingyued <jingyued@uci.edu>
2024-04-28 11:00:46 -0700

    Revert "retrive news data & display on home page"
    This reverts commit 4b914f8279aa1db0cbfe8e8973a6407e62b55e5a.

--- Chirp/src/app/shared/components/chirrup-list/chirrup-list.component.ts
+++ Chirp/src/app/shared/components/chirrup-list/chirrup-list.component.ts
@@ -1,32 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { GetChirrupsService } from '../../services/get-chirrups.service';
-import { Chirrup } from '../../../core/models/chirrup';
 
 @Component({
   selector: 'app-chirrup-list',
   templateUrl: './chirrup-list.component.html',
-  styleUrls: ['./chirrup-list.component.sass', '../chirrup-card/chirrup-card.component.sass']
+  styleUrls: ['./chirrup-list.component.sass']
 })
 export class ChirrupListComponent implements OnInit {
-  news: Chirrup[] = []; // This will hold an array of Chirrup objects
 
-  constructor(private getChirrupsService: GetChirrupsService) { }
+  constructor() { }
 
-  ngOnInit() {
-    this.getChirrupsService.getNews().subscribe({
-      next: (data: Chirrup[]) => { // Ensure this is typed as an array of Chirrup
-        this.news = data.map((item: Chirrup) => ({ // Use parentheses for the syntax
-          ...item,
-          islike: false
-        }));
-      },
-      error: (error) => {
-        console.error('There was an error!', error);
-      }
-    });
+  ngOnInit(): void {
   }
 
-  toggleHeartIcon(chirrup: Chirrup) { // It's better to type the parameter as Chirrup if that's what it's expected to be
-    chirrup.islike = !chirrup.islike; // This will toggle the like state of the chirrup
-  }
 }
