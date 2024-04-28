7d98035a1d7e43309a138f3164acf5c7fa2e81be
jingyued <jingyued@uci.edu>
2024-04-28 05:05:50 -0700

    retrive chirrups data

--- Chirp/src/app/app.module.ts
+++ Chirp/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     ButtonModule,
     ChirrupModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Chirp/src/app/shared/components/chirrup-list/chirrup-list.component.ts
+++ Chirp/src/app/shared/components/chirrup-list/chirrup-list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { GetChirrupsService } from '../../services/get-chirrups.service';
+import { Chirrup } from '../../../core/models/chirrup';
 
 @Component({
   selector: 'app-chirrup-list',
   templateUrl: './chirrup-list.component.html',
-  styleUrls: ['./chirrup-list.component.sass']
+  styleUrls: ['./chirrup-list.component.sass', '../chirrup-card/chirrup-card.component.sass']
 })
 export class ChirrupListComponent implements OnInit {
+  news: Chirrup[] = []; // This will hold an array of Chirrup objects
 
-  constructor() { }
+  constructor(private getChirrupsService: GetChirrupsService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getChirrupsService.getNews().subscribe({
+      next: (data: Chirrup[]) => { // Ensure this is typed as an array of Chirrup
+        this.news = data.map((item: Chirrup) => ({ // Use parentheses for the syntax
+          ...item,
+          islike: false
+        }));
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      }
+    });
   }
 
+  toggleHeartIcon(chirrup: Chirrup) { // It's better to type the parameter as Chirrup if that's what it's expected to be
+    chirrup.islike = !chirrup.islike; // This will toggle the like state of the chirrup
+  }
 }
