ab7a0f8ed7147a3d2e2e20171600138a594cb669
mercuryedge117 <mercucyedge@gmail.com>
2024-05-02 10:52:04 -0500

    handling error(temp)

--- Chirp/src/app/shared/services/auth.service.ts
+++ Chirp/src/app/shared/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs';
 import { User } from 'src/app/core/models/user';
+import { __exportStar, __read } from 'tslib';
 
 @Injectable({
   providedIn: 'root'
@@ -15,14 +16,20 @@ export class AuthService {
   loginAuth(email: string, password: string) {
     const url = this.apiUrl + 'login';
     this.http.post(url, { userEmail: email, password: password })
-      .subscribe((_resp: any) => this.token = _resp.bearerToken);
+      .subscribe({
+        next: (_resp: any) => this.token = _resp.bearerToken,
+        error: _err => console.log(_err.error)
+      });
   }
 
   registerUser(user: User) {
     // TODO: find a way to retrieve token
     const url = this.apiUrl + 'register/createNewAccount';
     let res = this.http.post(url, user, { observe: 'response' });
-    res.subscribe((_resp: any) => { })
+    res.subscribe({
+      next: _resp => {},
+      error: _err => console.log("status "+_err.status+" - "+_err.error)
+    })
   }
 
   // checkExistById() {
