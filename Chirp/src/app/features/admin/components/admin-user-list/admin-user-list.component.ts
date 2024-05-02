2a11890f4c16deed02ac5dd4435f4db349fa0b59
mercuryedge117 <mercucyedge@gmail.com>
2024-05-02 10:55:03 -0500

    testing lines, REVERT before merge

--- Chirp/src/app/features/admin/components/admin-user-list/admin-user-list.component.ts
+++ Chirp/src/app/features/admin/components/admin-user-list/admin-user-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/core/models/user';
+import { AuthService } from 'src/app/shared/services/auth.service';
 import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
@@ -23,7 +24,7 @@ export class AdminUserListComponent implements OnInit {
     "phone": 1234567890,
   };
 
-  constructor(private users: UserService) { }
+  constructor(private users: UserService, private auth: AuthService) { }
 
   ngOnInit(): void {
     this.users.getAllData().subscribe((res) => {
@@ -33,6 +34,11 @@ export class AdminUserListComponent implements OnInit {
 
   onAddNew(){
     console.log("gonna add new");
+    // manually switch for testing register func
+    // this.auth.registerUser({userName: "axnaxar", userEmail: "axnaxar@gmail.com", password: "aaaaaaaa"})
+    // this.auth.registerUser({userName: "naxar", userEmail: "naxar@ggg.com", password: "aaaaaaaa"})
+    // manually switch for testing login func
+    this.auth.loginAuth("axnaxar@gmail.com", "aaaaaaaa");
   }
 
   // TODO: try not display user when click delete
