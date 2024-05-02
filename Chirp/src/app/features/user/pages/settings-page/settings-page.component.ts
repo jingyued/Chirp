6712edb951138d3809139e758c9eda4f6422ec89
mercuryedge117 <mercucyedge@gmail.com>
2024-05-01 19:25:48 -0500

    major fix #1, temporary calling method

--- Chirp/src/app/features/user/pages/settings-page/settings-page.component.ts
+++ Chirp/src/app/features/user/pages/settings-page/settings-page.component.ts
@@ -1,14 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { DialogCommunicationService } from '../register-window/dialog-communication.service';
+import { ChangePasswordWindowComponent } from '../change-password-window/change-password-window.component';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-settings-page',
   templateUrl: './settings-page.component.html',
   styleUrls: ['./settings-page.component.sass']
 })
-export class SettingsPageComponent implements OnInit {
-  constructor() { }
+export class SettingsPageComponent implements OnInit, OnDestroy {
+
+  ref: DynamicDialogRef | undefined;
+  private unsubscribe$ = new Subject<void>();
+
+  constructor(    
+    private dialogService: DialogService,
+    private dialogCommunicationService: DialogCommunicationService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  onClickPassword() {
+
+    try {
+      this.ref = this.dialogService.open(ChangePasswordWindowComponent, {
+        width: '25rem',
+        showHeader: false,
+        contentStyle: {
+          "max-height": "600px",
+          "overflow": "auto",
+          "border-radius": "25px" // Optional: Add border-radius for rounded corners
+        }
+      });
+
+      this.dialogCommunicationService.registrationSuccess$.subscribe(() => {
+        this.closeDialog();
+      })
+    } catch (error) {
+      console.error('Error opening dialog:', error);
+    }
+  }
+
+  closeDialog() {
+    if (this.ref) {
+      this.ref.close();
+    }
+  }
 }
