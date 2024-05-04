10f62ac004aa9d565d77a77c7cbeeec8730c8de5
yueyingliang <liang.yuey@northeastern.edu>
2024-05-03 21:39:26 -0400

    redo-edit-profile-window&bind-form

--- Chirp/src/app/shared/services/user.service.ts
+++ Chirp/src/app/shared/services/user.service.ts
@@ -8,9 +8,22 @@ import { User } from 'src/app/core/models/user';
 export class UserService {
   userList: User[] = [];
 
+  currentUser: User = {
+    userName: "Felix",
+    password: "aabbccdd",
+    userEmail: "felix@gmail.com"
+  }
+
   constructor(private http: HttpClient) { }
 
   getAllData() {
     return this.http.get<User[]>('http://localhost:4231/api/users/getAllUsers');
+  }
+  getCurrentUser() {
+    return this.currentUser;
+  }
+  updateCurrentUser(updateUser: User) {
+    this.currentUser = updateUser;
+    this.getCurrentUser();
   }
 }
