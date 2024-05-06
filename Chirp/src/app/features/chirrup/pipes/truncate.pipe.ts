a9e656233933dcea130029e4ce3a8ef4fda22f0d
jingyued <jingyued@uci.edu>
2024-05-05 23:22:55 -0700

    display a single letter for user avatar

--- Chirp/src/app/features/chirrup/pipes/truncate.pipe.ts
+++ Chirp/src/app/features/chirrup/pipes/truncate.pipe.ts
@@ -4,9 +4,9 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'truncate'
 })
 export class TruncatePipe implements PipeTransform {
-  transform(value: string | undefined): string {
-    if (typeof value === 'string' && value.length > 20) {
-      return value.slice(0, 20) + '...';
+  transform(value: string | undefined, maxLength: number = 20): string {
+    if (typeof value === 'string' && value.length >= maxLength) {
+      return value.slice(0, maxLength) + '...';
     } else {
       return value ?? '';
     }
